5ff5947b7df3149080451245a3f26b87496382d3
Garv Shah <garvshah01@gmail.com>
2021-10-21 00:05:37 +0530

    hi this is my 2nd commit

--- lib/Map1.dart
+++ lib/Map1.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:wear/wear.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
-class GoogleMapScreen extends StatefulWidget{
+class GoogleMapScreen extends StatefulWidget {
   @override
   _GoogleMapScreenState createState() => _GoogleMapScreenState();
 }
 
-class _GoogleMapScreenState extends State<GoogleMapScreen>{
+class _GoogleMapScreenState extends State<GoogleMapScreen> {
   // Set<Marker> _markers = {};
   // void _onMapCreated(GoogleMapController controller){
   //   setState(() {
@@ -18,22 +17,22 @@ class _GoogleMapScreenState extends State<GoogleMapScreen>{
   //   });
   // }
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: WatchShape(builder: (context,shape,child){
-          return Container(child:
-      GoogleMap(
-        //onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-            target: LatLng(28.630577,77.372170),
-            zoom: 10
-        ),
-      )
-
-      );
-    },
-    child: AmbientMode(builder: (context,mode,child){
-     return Text("Ambient Mode");
-     },),));
+        body: WatchShape(
+      builder: (context, shape, child) {
+        return Container(
+            child: GoogleMap(
+          //onMapCreated: _onMapCreated,
+          initialCameraPosition:
+              CameraPosition(target: LatLng(28.630577, 77.372170), zoom: 16),
+        ));
+      },
+      child: AmbientMode(
+        builder: (context, mode, child) {
+          return Text("Ambient Mode");
+        },
+      ),
+    ));
   }
 }
